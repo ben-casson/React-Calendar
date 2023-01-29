51ef8fbaa1c7eb7498af2c95cbc477f48c4abfd4
Ben Casson <bcasson11@gmail.com>
2023-01-29 15:20:54 -0600

    Create date picker component

---
+++ src/DatePicker/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Calendar from './Calendar';
+import './index.scss';
+
+
+
+export default function DatePicker() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <section>
+            <div id='wrapper-date-picker'>
+                <div id='btn-container'>
+                    <button id='btn-previous-day' title='Go to the previous day'></button>
+                    <button
+                        id='btn-selected-day'
+                        onClick={() => {
+                            setIsOpen(!isOpen);
+                        }}
+                    >
+                        Today
+                    </button>
+                    <button id='btn-next-day' title='Go to the next day'></button>
+                </div>
+                {/* always display calendar, just make it small/hidden
+                        if isOpen then add size transition class to 'open' it */}
+                <Calendar isOpen={isOpen} />
+            </div>
+        </section>
+    );
+}
