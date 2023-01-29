ee18d0e425485813af391439803ab1f9bdb7dd93
Ben Casson <bcasson11@gmail.com>
2023-01-29 15:18:25 -0600

    Update imported component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.scss';
-import Calendar from './Calendar';
+import DatePicker from './DatePicker';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <Calendar />
+            <DatePicker />
         </>
     );
 }
