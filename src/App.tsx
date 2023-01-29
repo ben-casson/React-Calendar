27fa55dbd9ec6dbc1193408ad427532e30ae5677
Ben Casson <bcasson11@gmail.com>
2023-01-29 17:33:16 -0600

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.scss';
 import DatePicker from './DatePicker';
 
 function App() {
-    const [count, setCount] = useState(0);
 
     return (
         <>
