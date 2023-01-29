9ca2a29a340efdc0626e8a79718bc07dc9bc7399
Ben Casson <bcasson11@gmail.com>
2023-01-29 15:19:50 -0600

    Change name of imported file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './reset.css';
-import './index.css';
+import './global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
