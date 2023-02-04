04f66ef4bad95e229aa29e21ab4578e9ec63e1ad
Ben Casson <bcasson11@gmail.com>
2023-02-04 16:08:56 -0600

    Add date selection buttons

--- src/DatePicker/Calendar/index.tsx
+++ src/DatePicker/Calendar/index.tsx
@@ -1,9 +1,29 @@
 import './index.scss';
 
 interface CalendarProps {
-    isOpen: boolean,
+    isOpen: boolean;
+    selectedDate: string;
 }
 
-export default function Calendar({ isOpen }: CalendarProps ) {
-    return <div id='calendar' className={isOpen ? 'open' : 'closed'} ></div>;
+export default function Calendar({ isOpen, selectedDate }: CalendarProps) {
+    let date = new Date();
+    console.log(date.toDateString());
+
+    let monthYearText = selectedDate.slice(4, 7) + ' ' + selectedDate.slice(11);
+
+    return (
+        <div id='calendar' className={isOpen ? 'open' : 'closed'}>
+            <div id='content'>
+                <div id='month-year-week-container'>
+                    <div id='month-year-container'>
+                        <button id='btn-previous-year'></button>
+                        <button id='btn-previous-month'></button>
+                        <div>{monthYearText}</div>
+                        <button id='btn-next-month'></button>
+                        <button id='btn-next-year'></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
