322f73a6cd52c9f75a1ef00a55d72c09f0cc4e95
Ben Casson <bcasson11@gmail.com>
2023-02-03 21:07:09 -0600

    Add enum for number of days in each month

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import { useState } from 'react';
 import './App.scss';
 import DatePicker from './DatePicker';
 
+enum DaysInMonth {
+    Jan = 31,
+    Feb = 28,
+    FebLeap = 29,
+    Mar = 31,
+    Apr = 30,
+    May = 31,
+    Jun = 30,
+    Jul = 31,
+    Aug = 31,
+    Sep = 30,
+    Oct = 31,
+    Nov = 30,
+    Dec = 31,
+}
+
 function App() {
 
     return (
