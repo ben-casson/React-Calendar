4cf0f48f79b855da237a0d16dbe30a81512d1163
Ben Casson <bcasson11@gmail.com>
2023-02-04 08:58:58 -0600

    Create file for enums

---
+++ src/enums.ts
@@ -0,0 +1,25 @@
+export enum DaysInMonth {
+    Jan = 31,
+    Feb = 28,
+    FebLeap = 29,
+    Mar = 31,
+    Apr = 30,
+    May = 31,
+    Jun = 30,
+    Jul = 31,
+    Aug = 31,
+    Sep = 30,
+    Oct = 31,
+    Nov = 30,
+    Dec = 31,
+}
+
+export enum daysOfWeek {
+    Sun,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat
+}
