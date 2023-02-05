d9fa714760d58e80ecf8bf57cc71bd1384e3096a
Ben Casson <bcasson11@gmail.com>
2023-02-05 14:19:21 -0600

    Pass a set state prop to DatePicker component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import DatePicker from './DatePicker';
 
 
 function App() {
+    let date = new Date();
+    console.log(date.toDateString());
+
     const [selectedDate, setSelectedDate] = useState('Sat Feb 04 2023');
+    // const [selectedDate, setSelectedDate] = useState(new Date().toDateString());
 
 
     return (
         <>
-            <DatePicker selectedDate={selectedDate} />
+            <DatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
         </>
     );
 }
