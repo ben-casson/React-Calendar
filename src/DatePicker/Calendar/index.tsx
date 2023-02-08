8ac4acddc26415f9802a9a09a85ceeecf2613628
Ben Casson <bcasson11@gmail.com>
2023-02-07 19:58:23 -0600

    Use correct imports

--- src/DatePicker/Calendar/index.tsx
+++ src/DatePicker/Calendar/index.tsx
@@ -8,9 +8,11 @@ interface ICalendarProps {
     isOpen: boolean;
     daysArray: Array<JSX.Element>;
     setDaysArray: Dispatch<SetStateAction<Array<JSX.Element>>>;
-    getPreviousMonth: Function;
+    changeMonth: Function;
     selectedDate: string;
     setSelectedDate: Dispatch<SetStateAction<string>>;
+    calendarDisplayDate: string;
+    setCalendarDisplayDate: Dispatch<SetStateAction<string>>;
 }
 
 type Day = {
@@ -19,30 +21,38 @@ type Day = {
     content: string;
 };
 
-export default function Calendar({ isOpen, daysArray, setDaysArray, getPreviousMonth, selectedDate, setSelectedDate }: ICalendarProps) {
-
-    let dayOfWeek = selectedDate.slice(0, 3);
-    let currentMonth = selectedDate.slice(4, 7);
-    let currentYear = selectedDate.slice(11);
+export default function Calendar({
+    isOpen,
+    daysArray,
+    setDaysArray,
+    changeMonth,
+    selectedDate,
+    setSelectedDate,
+    calendarDisplayDate,
+    setCalendarDisplayDate,
+}: ICalendarProps) {
+    let dayOfWeek = calendarDisplayDate.slice(0, 3);
+    let currentMonth = calendarDisplayDate.slice(4, 7);
+    let currentYear = calendarDisplayDate.slice(11);
     let monthLength = DaysInMonth[currentMonth as keyof typeof DaysInMonth];
     let firstDayOfMonth = new Date(`01 ${currentMonth} ${currentYear}`).getDay();
-    console.log(firstDayOfMonth)
+    console.log(firstDayOfMonth);
+
     useEffect(() => {
         let tempArray = [...daysArray];
         for (let i = 0; i <= monthLength + firstDayOfMonth - 1; i++) {
-            if (i < firstDayOfMonth ) {
-                tempArray.push(<div key={i}></div>)
+            if (i < firstDayOfMonth) {
+                tempArray.push(<div key={i}></div>);
                 // console.log(i)
-            }
-            else {
-                tempArray.push(<Day key={i} number={i - firstDayOfMonth + 1} />)
+            } else {
+                tempArray.push(<Day key={i} number={i - firstDayOfMonth + 1} />);
             }
         }
         setDaysArray(tempArray);
-        console.log(daysArray)
-    }, [selectedDate]);
+        console.log(daysArray);
+    }, [calendarDisplayDate]);
 
-    let monthYearText = selectedDate.slice(4, 7) + ' ' + selectedDate.slice(11);
+    let monthYearText = calendarDisplayDate.slice(4, 7) + ' ' + calendarDisplayDate.slice(11);
 
     return (
         <div id='calendar' className={isOpen ? 'open' : 'closed'}>
@@ -50,9 +60,12 @@ export default function Calendar({ isOpen, daysArray, setDaysArray, getPreviousM
                 <div id='month-year-week-container'>
                     <div id='month-year-container'>
                         <button id='btn-previous-year'></button>
-                        <button id='btn-previous-month' onClick={() => getPreviousMonth()}></button>
+                        <button
+                            id='btn-previous-month'
+                            onClick={() => changeMonth('previous')}
+                        ></button>
                         <div>{monthYearText}</div>
-                        <button id='btn-next-month'></button>
+                        <button id='btn-next-month' onClick={() => changeMonth('next')}></button>
                         <button id='btn-next-year'></button>
                     </div>
                     <div id='week-container'>
