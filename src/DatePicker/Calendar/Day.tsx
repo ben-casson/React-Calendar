d61a9a3e1881b44281664dcfafbd8ede1ebdb535
Ben Casson <bcasson11@gmail.com>
2023-02-10 17:30:55 -0600

    Move handleClick function to parent component

--- src/DatePicker/Calendar/Day.tsx
+++ src/DatePicker/Calendar/Day.tsx
@@ -11,6 +11,9 @@ interface IDayProps {
     selectedDate: string;
     setSelectedDate: Dispatch<SetStateAction<string>>;
     calendarDisplayDate: string;
+    handleClick: Function;
+    selectedDay: string;
+    setSelectedDay: Dispatch<SetStateAction<string>>;
 }
 
 export default function Day({
@@ -22,33 +25,45 @@ export default function Day({
     selectedDate,
     setSelectedDate,
     calendarDisplayDate,
+    handleClick,
+    selectedDay,
+    setSelectedDay,
 }: IDayProps) {
-    const [isSelected, setIsSelected] = useState(false);
-
-    function handleClick() {
-        let month = calendarDisplayDate.slice(4, 7);
-        let year = calendarDisplayDate.slice(11);
-        let day = number < 10 ? '0' + number.toString() : number;
-        let dayofWeek = daysOfWeek[new Date(`${day} ${month} ${year}`).getDay()];
-        setSelectedDate(`${dayofWeek + ' ' + month + ' ' + day + ' ' + year}`);
-        // setDayStyle('btn-day selected');
-        console.log('selected day' + number);
-        console.log('1: ' + new Date().toDateString());
-        console.log('2: ' + calendarDisplayDate);
-    }
+    // function handleClick() {
+    //     let month = calendarDisplayDate.slice(4, 7);
+    //     let year = calendarDisplayDate.slice(11);
+    //     let day = number < 10 ? '0' + number.toString() : number;
+    //     let dayofWeek = daysOfWeek[new Date(`${day} ${month} ${year}`).getDay()];
+    //     setSelectedDate(`${dayofWeek + ' ' + month + ' ' + day + ' ' + year}`);
+    //     // setDayStyle('btn-day selected');
+    //     console.log('selected day' + number);
+    //     console.log('1: ' + new Date().toDateString());
+    //     console.log('2: ' + calendarDisplayDate);
+    // }
 
     return (
         <button
-            onClick={() => handleClick()}
+            onClick={() =>
+                handleClick(
+                    number,
+                    calendarDisplayDate.slice(0, 8) +
+                        number.toString() +
+                        calendarDisplayDate.slice(10)
+                )
+            }
             className={
                 'btn-day' +
                 // (parseInt(selectedDate.slice(8, 10)) === number &&
                 // calendarDisplayDate.slice(4, 7) === selectedDate.slice(4, 7) &&
                 // calendarDisplayDate.slice(11) === selectedDate.slice(11)
-                (new Date().toDateString() === calendarDisplayDate.slice(0, 8) + number + calendarDisplayDate.slice(10)
+                (new Date().toDateString() ===
+                calendarDisplayDate.slice(0, 8) + number + calendarDisplayDate.slice(10)
                     ? ' today'
                     : '') +
-                (isSelected ? ' selected' : '')
+                (selectedDay ===
+                calendarDisplayDate.slice(0, 8) + number.toString() + calendarDisplayDate.slice(10)
+                    ? ' selected'
+                    : ' hover')
             }
         >
             {number}
